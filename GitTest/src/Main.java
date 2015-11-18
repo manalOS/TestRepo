03c3089d3cc603ad265e7785796e11832227c9cb
ManalOS <436203200@student.ksu.edu.sa>
2015-11-18 14:26:58 +0300

    third commit

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,6 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test Java second time");
+        System.out.println("Test Java third time");
     }
 }
