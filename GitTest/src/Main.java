c6eee9d147bdae79fd7cc1b9d53867e9be353224
sara <salghunaim@kacst.edu.sa>
2015-11-26 22:11:25 +0300

    sara Edit #3  after pull

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test Pull request");
         System.out.println("branch, pull request");
-        System.out.println("Sara Edit #3");
+        System.out.println("Sara Edit #3  after pull");
         
     }
 }
